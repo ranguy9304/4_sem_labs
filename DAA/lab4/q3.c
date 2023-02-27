37bc615768f12a7eb0b080e46c68ee659116b671
Rudra Patel <88544472+ranguy9304@users.noreply.github.com>
2023-02-27 16:21:35 +0530

    Update q3.c

--- DAA/lab4/q3.c
+++ DAA/lab4/q3.c
@@ -1,44 +1,166 @@
-#include <iostream>
-#include <vector>
-#include <queue>
 
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node* node_ptr;
+typedef struct node
+{
+    int key;
+    
+    node_ptr *links;
+    int no_links
+}node;
 
-const int MAX_N = 100;
 
-int n; // number of vertices
-vector<int> adj_list[MAX_N]; // adjacency list representation of the graph
-bool visited[MAX_N]; // array to keep track of visited vertices
-
-void bfs(int v) {
-    queue<int> q;
-    q.push(v); // push the starting vertex onto the queue
-    visited[v] = true;
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        cout << "Visited vertex " << u << endl;
-        for (int i = 0; i < adj_list[u].size(); i++) {
-            int w = adj_list[u][i];
-            if (!visited[w]) {
-                q.push(w);
-                visited[w] = true;
-            }
-        }
+node_ptr init_node(int no_vert,int val){
+    node_ptr node = (node_ptr)malloc(sizeof(node));
+    node->key=val;
+    int links;
+    printf("enter the number of connections for [ %d ] : ",val);
+    scanf("%d",&links);
+    if(links>=no_vert){
+        printf("ERR : links not possible : enter within range [ 0 - %d ]\ntry again :",no_vert-1);
+        scanf("%d",&links);
     }
+    node->no_links=links;
+    node->links=(node_ptr*)malloc(links*sizeof(node_ptr));
+
+    return node;
+
+}
+void connect(node_ptr node,node_ptr *lookup,int no_vert){
+    int ver;
+    int count=0;
+    printf("\n\n#####  %d  ######\n",node->key);
+    printf("Enter links in ascending order \n");
+    printf("which are the linked vertices : [ no links = %d ]\n",node->no_links);
+    for(int i=0;i<node->no_links;i++){
+        scanf("%d",&ver);
+        if(ver>=no_vert){
+            printf("ERR : vertice entered incorectly enter within range [ 0 - %d ]\ntry again ",no_vert-1);
+            scanf("%d",&ver);}
+
+        node->links[count]=lookup[ver];
+        count++;
+    }
+
+}
+node_ptr* make_lookup(int no_vert){
+    node_ptr node;
+    node_ptr *lookup=(node_ptr*)malloc(no_vert*sizeof(node_ptr));
+
+
+    for(int i=0;i<no_vert;i++){
+        node=init_node(no_vert,i);
+        lookup[i]=node;
+    }
+    return lookup;
+
+}
+void print_links(node_ptr n){
+    printf("------ links for [ %d ] ------ \n",n->key);
+    for(int i =0 ;i< n->no_links;i++){
+        printf(" %d ",n->links[i]->key);
+    }
+    printf("\n");
 }
 
-int main() {
-    n = 6;
-    adj_list[0].push_back(1);
-    adj_list[0].push_back(2);
-    adj_list[1].push_back(2);
-    adj_list[2].push_back(0);
-    adj_list[2].push_back(3);
-    adj_list[3].push_back(3);
-    adj_list[3].push_back(4);
-    adj_list[4].push_back(5);
-    adj_list[5].push_back(5);
-    bfs(2); // start from vertex 2
-    return 0;
+int g[100][100];
+int V;
+int visited[100];
+void print_matrix(node_ptr* lookup,int no_vert){
+    printf("\n*****  ADA MATRIX *****\n");
+    int count =0;
+    for(int i=0;i<no_vert;i++){
+        count=1;
+        for(int j=0;j<no_vert;j++){
+            if(lookup[i]->no_links==0){
+                continue;
+            }
+            if(lookup[i]->links[count-1]->key == j){
+                g[i][j]=1;
+                printf(" 1 ");
+                if(lookup[i]->no_links>count)
+                    count++;
+            }
+            else{
+                g[i][j]=1;
+
+                printf(" 0 ");
+            }
+                        
+        }
+    printf("\n");
+    }
+    
+
+}
+
+
+
+int queue[100], f = 0, r = 0;
+
+void enqueue(int v)
+{
+	queue[r++] = v;
+}
+
+int dequeue()
+{
+	if(f == r)
+	{
+		return -1;
+	}
+	
+	return queue[f++];
+}
+
+void bfsv(int v)
+{
+	printf("Visiting %d\n", v);
+	visited[v] = 1;
+	
+	int i;
+	
+	for(i = 0; i < V; ++i)
+	{
+		if(!visited[i] && g[v][i] && i != v)
+		{
+			enqueue(i);
+		}
+	}
+}
+
+void bfs()
+{
+	int i, x;
+	enqueue(0);
+	
+	do
+	{
+		x = dequeue();
+		
+		if(x != -1 && !visited[x])
+		{
+			bfsv(x);
+		}
+	}while (x != -1);
+}
+
+void main(){
+    int no_vert=4;
+    V=4;
+    printf(" the existing vertices are \n");
+    for(int i=0;i<no_vert;i++){
+        printf(" %d ",i);
+    }
+    printf("\n");
+    node_ptr *lookup=make_lookup(no_vert);
+    for(int i=0;i<no_vert;i++){
+        connect(lookup[i],lookup,no_vert);
+    }
+    for(int i=0;i<no_vert;i++){
+        print_links(lookup[i]);
+    }
+    print_matrix(lookup,no_vert);
+    bfs();
 }
